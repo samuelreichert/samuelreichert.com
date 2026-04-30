4bf329a7c262a231daaab43b95e3a8b819342467
Samuel Reichert <samuelreichertt@gmail.com>
2026-04-30 12:32:05 +0200

    feat: scaffold Astro + React + TypeScript with Vercel adapter

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://samuelreichert.com',
+  integrations: [react(), sitemap()],
+  output: 'static',
+  adapter: vercel(),
+  trailingSlash: 'never',
+});
